ae6357c65713d12f18b502e6079a6f07b488c6b0
Mark Spannbauer <msmarkieer@gmail.com>
2020-04-20 10:56:32 -0500

    completed code challenge

--- server/routes/zoo.router.js
+++ server/routes/zoo.router.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 router.get('/', (req, res) => {
-    // YOUR CODE HERE
+    let queryText = `SELECT * FROM "species" 
+        JOIN "class" ON "class_id" = "class"."id"
+        ORDER BY "species"."id";`;
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('error selecting * from species', error);
+        res.sendStatus(500);
+    });
     
 });
 
